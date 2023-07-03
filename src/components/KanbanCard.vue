d95cf0fdf6453902dd663c46d03407acec6cf45a
otisnth <otisnth@gmail.com>
2023-07-03 21:30:13 +0300

    add images, update card, deploy to pages

--- src/components/KanbanCard.vue
+++ src/components/KanbanCard.vue
@@ -1,9 +1,15 @@
 <template>
     <div class="kanban-card">
-        <div class="kanban-card__number">Number</div>
+        <div class="kanban-card__number">N-930</div>
         <h4 class="kanban-card__title">Title</h4>
-        <div class="kanban-card__type">Type</div>
-        <div class="kanban-card__worker">Worker</div>
+        <div class="kanban-card__property">
+            <img class="kanban-card__icon" src="@/assets/images/bug.svg" alt="Дефект" title="Дефект">
+            <img class="kanban-card__icon" src="@/assets/images/prio-low.svg" alt="Низкий приоритет" title="Низкий приоритет">
+        </div>
+        <div class="kanban-card__worker">
+            <img class="kanban-card__avatar" src="@/assets/images/avatar1.png" alt="Аватар">
+            <p class="kanban-card__worker-name">Lastname Firstname</p>
+        </div>
     </div>
 </template>
 
@@ -19,6 +25,7 @@ export default {
 <style lang="scss" scoped>
 
 .kanban-card {
+    width: 200px;
     padding: 16px;
     border-radius: 8px;
     border: 1px solid #DDD;
@@ -36,12 +43,23 @@ export default {
         font-weight: 600;
     }
 
-    .kanban-card__type {
-        
+    .kanban-card__property {
+        display: flex;
+        gap: 4px;
+    }
+
+    .kanban-card__icon {
+        width: 18px;
     }
 
     .kanban-card__worker {
-        
+        display: flex;
+        gap: 4px;
+    }
+
+    .kanban-card__avatar {
+        width: 18px;
+        border-radius: 50%;
     }
 }
 
